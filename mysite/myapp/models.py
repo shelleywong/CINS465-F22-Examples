4d72cf196777739b13f72da5174bfa506c8b4982
Shelley Wong <swong26@csuchico.edu>
2022-09-20 15:07:29 -0700

    adds first model

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+import datetime
 
 # Create your models here.
+class QuestionModel(models.Model):
+    question_text = models.CharField(max_length=280)
+    pub_date = models.DateTimeField(default=datetime.date.today)
+
+    def __str__(self):
+        return "ID: " + str(self.id) + ", " + self.question_text + ", " + str(self.pub_date.strftime("%d %b %Y"))
--- mysite/myapp/views.py
+++ mysite/myapp/views.py
@@ -1,11 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from . import models
+
 # Create your views here.
 def index(request, page=0):
     #return HttpResponse("Hello, world! CINS 465!")
     page_list = list(range(page*10, page*10 + 10, 1))
     squares_list = [x**2 for x in range(10)]
+    q_list = models.QuestionModel.objects.all()
     context = {
         'first_name': 'Shelley',
         'last_name': 'Wong',
@@ -15,5 +18,6 @@ def index(request, page=0):
         'page_list': page_list,
         'prev_page': page - 1,
         'next_page': page + 1,
+        'q_list': q_list,
     }
     return render(request, "index.html", context=context)
