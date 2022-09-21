94dd679341d950e7bd2a24c36782425a9c0950a6
Shelley Wong <swong26@csuchico.edu>
2022-09-20 17:02:51 -0700

    fixed issue with DateTimeField

--- mysite/myapp/models.py
+++ mysite/myapp/models.py
@@ -4,7 +4,8 @@ import datetime
 # Create your models here.
 class QuestionModel(models.Model):
     question_text = models.CharField(max_length=280)
-    pub_date = models.DateTimeField(default=datetime.date.today)
+   # pub_date = models.DateTimeField(default=datetime.date.today)
+    pub_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return "ID: " + str(self.id) + ", " + self.question_text + ", " + str(self.pub_date.strftime("%d %b %Y"))
